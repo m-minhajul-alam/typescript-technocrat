d55365f093760103cf0f1643cc5207cc1b5358d4
Minhajul-alam <alam.minhajul@outlook.com>
2024-05-02 15:47:13 +0600

    Spread and Rest Operator

---
+++ module1/src/1.7.ts
@@ -0,0 +1,40 @@
+// Spread Operator
+// Rest Operator
+// Distructuring
+{
+  //
+  // Spread Operator
+  const freandList1 = ["Rashed", "Akib", "Minhaj"];
+  const freandList2 = ["Sakib", "Nakib", "Dakib"];
+  freandList1.push(...freandList2);
+  console.log(freandList1);
+
+  const userList1 = {
+    user1: "Hamid",
+    user2: "Sakib",
+    user3: "Yaqub",
+  };
+  const userList2 = {
+    user4: "Akib",
+    user5: "Nkib",
+    user6: "Lakib",
+  };
+  const allUserList = {
+    ...userList2,
+    ...userList1,
+  };
+  console.log(allUserList);
+
+  // Rest Operator
+  //   const greetFriends = (friend1: string, friend2: string, friend3: string) => {
+  //     console.log(`Hi ${friend1}, ${friend2} and ${friend3}`);
+  //   };
+  //   greetFriends("Akib", "Sakib", "Dakib");
+
+  const greetFriends = (...friends: string[]) => {
+    friends.forEach((friend) => console.log(`Hi ${friend}`));
+  };
+  greetFriends("Akib", "Sakib", "Dakib", "Hakib");
+
+  //
+}
