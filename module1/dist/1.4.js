b2b6f1e1f6ed0dc6f45a68619613590bebff9eed
Minhajul-alam <alam.minhajul@outlook.com>
2024-05-02 09:39:51 +0600

    Basic data type

---
+++ module1/dist/1.4.js
@@ -0,0 +1,45 @@
+"use strict";
+{
+    //
+    //Basic Data Type
+    // Primitive:
+    // 1. Number
+    // 2. String
+    // 3. Boolean
+    // 4. Null
+    // 5. Undefined
+    // 6. Symbol
+    // Non Primitive Types
+    // 1. Array
+    // 2. Tuple
+    // 3. Object
+    // string
+    var name_1 = "alam";
+    // number
+    var rool = 123;
+    // boolean
+    var isAdmin = true;
+    // undefined
+    var x = undefined;
+    // null
+    var y = null;
+    // any
+    // let d;
+    var d = void 0;
+    d = 123;
+    // d = "abc";
+    // d = true;
+    // console.log(d);
+    // Array
+    var friendList = ["rashed", "rohmot", "arfat"];
+    var roolList = [1, 2, 3, 4];
+    // roolList.push(5);
+    // roolList.push("ss");
+    // console.log(roolList);
+    // tuple --> It's like a Array --It's Order type value
+    var rools = [34, 45, 23];
+    var nameAgeIsAdmin = ["mr. x", 37, true];
+    console.log(nameAgeIsAdmin);
+    //   nameAge[0] = 22;
+    //
+}
---
+++ module1/src/1.4.ts
@@ -0,0 +1,59 @@
+{
+  //
+  //Basic Data Type
+
+  // Primitive:
+  // 1. Number
+  // 2. String
+  // 3. Boolean
+  // 4. Null
+  // 5. Undefined
+  // 6. Symbol
+
+  // Non Primitive Types
+  // 1. Array
+  // 2. Tuple
+  // 3. Object
+
+  // string
+  let name: string = "alam";
+
+  // number
+  let rool: number = 123;
+
+  // boolean
+  let isAdmin: boolean = true;
+
+  // undefined
+  let x: undefined = undefined;
+
+  // null
+  let y: null = null;
+
+  // any
+  // let d;
+  let d: number;
+
+  d = 123;
+  // d = "abc";
+  // d = true;
+  // console.log(d);
+
+  // Array
+  let friendList: string[] = ["rashed", "rohmot", "arfat"];
+
+  let roolList: number[] = [1, 2, 3, 4];
+
+  // roolList.push(5);
+  // roolList.push("ss");
+  // console.log(roolList);
+
+  // tuple --> It's like a Array --It's Order type value
+  let rools: [number, number, number] = [34, 45, 23];
+
+  let nameAgeIsAdmin: [string, number, boolean] = ["mr. x", 37, true];
+  //   nameAge[0] = 22;
+  //   console.log(nameAgeIsAdmin);
+
+  //
+}
